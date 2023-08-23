9d1baad9a91cad1743db67474b76e76cec222960
ridhocatur <silverhedgie@gmail.com>
2023-08-23 12:22:43 +0800

    cetakdata form done

--- resources/views/pengadaan-perangkat/pdf.blade.php
+++ resources/views/pengadaan-perangkat/pdf.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Laporan</title>
+    <title>Laporan Pengadaan Perangkat</title>
 </head>
 <body>
     <center>
@@ -27,7 +27,7 @@
         </table>
     </center>
     <hr align="right" style="height:5px;border:none;color:#333;background-color:#333;">
-    <h5 class="text text-center">LAPORAN DATA WIFI PUBLIK</h5>
+    <h5 class="text text-center">LAPORAN DATA PENGADAAN PERANGKAT</h5>
     <div id="content">
         <table class="table table-bordered">
             <thead>
